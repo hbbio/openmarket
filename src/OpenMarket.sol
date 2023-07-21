aef5eb7abf95cace61a3e6eff7b13b9799cc262b
Henri Binsztok <808274+hbbio@users.noreply.github.com>
2023-07-21 16:55:01 +0200

    OpenMarket: check seller

--- src/OpenMarket.sol
+++ src/OpenMarket.sol
@@ -10,11 +10,16 @@ contract NFTMarketplace is Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
 
-    mapping(uint256 => uint256) private _tokenPrice;
+    mapping(uint256 => uint256) public tokenPrice;
+    mapping(uint256 => address) private _tokenSeller;
     IERC721Enumerable private _existingCollection;
 
     event NFTListed(uint256 indexed tokenId, uint256 price);
-    event NFTPriceUpdated(uint256 indexed tokenId, uint256 price);
+    event NFTPriceUpdated(
+        uint256 indexed tokenId,
+        address seller,
+        uint256 price
+    );
     event NFTSold(
         uint256 indexed tokenId,
         address indexed seller,
@@ -26,33 +31,34 @@ contract NFTMarketplace is Ownable {
         _existingCollection = IERC721Enumerable(existingCollection);
     }
 
-    function mintNFT(string memory tokenURI, uint256 price) external onlyOwner {
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _existingCollection.safeTransferFrom(
-            msg.sender,
-            address(this),
-            tokenId
-        );
-        _tokenPrice[tokenId] = price;
-        emit NFTListed(tokenId, price);
-    }
+    // function mintNFT(string memory tokenURI, uint256 price) external onlyOwner {
+    //     uint256 tokenId = _tokenIdCounter.current();
+    //     _tokenIdCounter.increment();
+    //     _existingCollection.safeTransferFrom(
+    //         msg.sender,
+    //         address(this),
+    //         tokenId
+    //     );
+    //     _tokenPrice[tokenId] = price;
+    //     emit NFTListed(tokenId, price);
+    // }
 
     function setPrice(uint256 tokenId, uint256 price) external {
-        require(_tokenPrice[tokenId] > 0, "NFT is not listed for sale");
+        // require(_tokenPrice[tokenId] > 0, "NFT is not listed for sale");
         require(
             _existingCollection.ownerOf(tokenId) == msg.sender,
             "You are not the owner"
         );
-        _tokenPrice[tokenId] = price;
-        emit NFTPriceUpdated(tokenId, price);
+        tokenPrice[tokenId] = price;
+        _tokenSeller[tokenId] = msg.sender;
+        emit NFTPriceUpdated(tokenId, msg.sender, price);
     }
 
     function buyNFT(uint256 tokenId) external payable {
-        require(_tokenPrice[tokenId] > 0, "NFT is not listed for sale");
+        require(tokenPrice[tokenId] > 0, "NFT is not listed for sale");
         require(
-            _existingCollection.ownerOf(tokenId) != address(this),
-            "NFT is not listed for sale"
+            _existingCollection.ownerOf(tokenId) == _tokenSeller[tokenId],
+            "NFT ownership changed"
         );
         require(
             _existingCollection.getApproved(tokenId) == address(this) ||
@@ -60,20 +66,21 @@ contract NFTMarketplace is Ownable {
                     _existingCollection.ownerOf(tokenId),
                     address(this)
                 ),
-            "You are not approved to buy this NFT"
+            "NFT not approved for sale"
         );
-        require(msg.value >= _tokenPrice[tokenId], "Insufficient payment");
+        require(msg.value >= tokenPrice[tokenId], "Insufficient payment");
 
         address seller = _existingCollection.ownerOf(tokenId);
         _existingCollection.safeTransferFrom(seller, msg.sender, tokenId);
-        uint256 price = _tokenPrice[tokenId];
-        _tokenPrice[tokenId] = 0; // Reset price after purchase
+        uint256 price = tokenPrice[tokenId];
+        tokenPrice[tokenId] = 0; // Reset price after purchase
+        _tokenSeller[tokenId] = address(0);
         payable(seller).transfer(price);
         emit NFTSold(tokenId, seller, msg.sender, price);
     }
 
     function getTokenPrice(uint256 tokenId) external view returns (uint256) {
-        return _tokenPrice[tokenId];
+        return tokenPrice[tokenId];
     }
 
     function getExistingCollection() external view returns (address) {

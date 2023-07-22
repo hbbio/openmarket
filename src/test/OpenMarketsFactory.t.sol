007b44236fd5872b94a75e52b4d13fd91fa61580
Henri Binsztok <808274+hbbio@users.noreply.github.com>
2023-07-22 11:26:38 +0200

    test: OpenMarketsFactoryTest not twice

--- src/test/OpenMarketsFactory.t.sol
+++ src/test/OpenMarketsFactory.t.sol
@@ -5,10 +5,11 @@ import "ds-test/test.sol";
 
 import "@erc721a/contracts/ERC721A.sol";
 
-import "./Azuki.t.sol";
-import "./User.t.sol";
 import "../OpenMarket.sol";
 import "../OpenMarketsFactory.sol";
+
+import "./Azuki.t.sol";
+import "./User.t.sol";
 
 contract OpenMarketsFactoryTest is DSTest {
     Azuki coll;
@@ -25,5 +26,10 @@ contract OpenMarketsFactoryTest is DSTest {
         address[] memory list = fact.getAllMarketplaces();
         assertEq(list.length, 1);
         assertEq(list[0], fact.getMarketplace(address(coll)));
+    }
+
+    function testFailMarketTwice() public {
+        fact.createMarketplace(address(coll));
+        fact.createMarketplace(address(coll));
     }
 }

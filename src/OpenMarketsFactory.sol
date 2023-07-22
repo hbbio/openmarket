fc8856dc1da99ca86b0d914b19e597f891d238cc
Henri Binsztok <808274+hbbio@users.noreply.github.com>
2023-07-22 11:23:18 +0200

    test: testCreateMarket ok

--- src/OpenMarketsFactory.sol
+++ src/OpenMarketsFactory.sol
@@ -6,7 +6,7 @@ pragma solidity 0.8.19;
 import "./OpenMarket.sol";
 
 // is Ownable
-contract NFTMarketplaceFactory {
+contract OpenMarketsFactory {
     mapping(address => address) private _collectionToMarketplace;
     address[] private _marketplaces;
 
--- src/test/OpenMarketsFactory.t.sol
+++ src/test/OpenMarketsFactory.t.sol
@@ -9,3 +9,21 @@ import "./Azuki.t.sol";
 import "./User.t.sol";
 import "../OpenMarket.sol";
 import "../OpenMarketsFactory.sol";
+
+contract OpenMarketsFactoryTest is DSTest {
+    Azuki coll;
+    OpenMarketsFactory fact;
+
+    function setUp() public {
+        fact = new OpenMarketsFactory();
+        coll = new Azuki();
+        emit log_named_address("sender", msg.sender);
+    }
+
+    function testCreateMarket() public {
+        fact.createMarketplace(address(coll));
+        address[] memory list = fact.getAllMarketplaces();
+        assertEq(list.length, 1);
+        assertEq(list[0], fact.getMarketplace(address(coll)));
+    }
+}
